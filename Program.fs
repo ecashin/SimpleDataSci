360181aad7ef18a431e1605237c36841cd4ddb5b
Ed Cashin <ed.cashin@acm.org>
2021-02-10 19:39:13 -0500

    Get dummy-variable labels

--- Program.fs
+++ Program.fs
@@ -11,5 +11,12 @@ let main argv =
         printfn "number of rows:%d, cols:%d" nRow nCol
         if data.RowCount > 0 then
             printfn "first row: %A" (data.GetRowAt 0)
+            let colNames =
+                data.ColumnKeys
+                |> Seq.toArray
+            // There are seven categories, each with a dummy variable.
+            let labels =
+                data.Columns.[colNames.[(nCol - 7)..(nCol - 1)]]
+            printfn "%dx%d %A" labels.RowCount labels.ColumnCount (labels.GetRowAt(0))
         0
     | _ -> 1
