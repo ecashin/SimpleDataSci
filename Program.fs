812dae3baa10de2ea4da429508fef8c5b7081ee0
Ed Cashin <ed.cashin@acm.org>
2021-02-14 15:21:07 -0500

    Rebalance data categorically

--- Program.fs
+++ Program.fs
@@ -1,12 +1,42 @@
 // https://archive.ics.uci.edu/ml/datasets/Amphibians
-open System
+open MathNet.Numerics.Random
 open Deedle
+
+let shuffleOrder n =
+    // inspred by
+    // http://www.fssnip.net/kS/title/Sample-for-traintest-sets-in-Deedle-Frames
+    let rng = SystemRandomSource.Default
+    seq { 1..n }
+    |> Seq.map (fun i -> (i, rng.Next()))
+    |> Seq.sortBy snd
+    |> Seq.map fst
+
+let rebalance (catSums: Series<string,float>) (data: Frame<int,string>) =
+    let n = Stats.min catSums |> int
+    let order: int list =
+        shuffleOrder data.RowCount
+        |> Seq.take n
+        |> Seq.toList
+    let balanced =
+        catSums.Keys
+        |> Seq.collect (fun label ->
+            data
+            |> Frame.filterRowsBy label 1
+            |> Frame.sliceRows order
+            |> (fun df -> df.GetRows() |> Series.values)
+        )
+        |> Series.ofValues
+        |> Frame.ofRows
+    printfn "balanced %A has rows x cols: %dx%d" balanced balanced.RowCount balanced.ColumnCount
+    balanced
 
 [<EntryPoint>]
 let main argv =
     match argv with
     | [|dataCsvFileName|] ->
-        let data = Frame.ReadCsv(dataCsvFileName, separators=";", inferTypes=true)
+        let data =
+            Frame.ReadCsv(dataCsvFileName, separators=";", inferTypes=true)
+            |> Frame.indexRowsOrdinally
         let nRow, nCol = data.RowCount, data.ColumnCount
         printfn "number of rows:%d, cols:%d" nRow nCol
         if data.RowCount > 0 then
@@ -26,5 +56,6 @@ let main argv =
                 |> Stats.sum
             // Below it shows the data isn't balanced.
             printfn "category representative counts: %A" catSums
+            rebalance catSums data |> ignore
         0
     | _ -> 1
