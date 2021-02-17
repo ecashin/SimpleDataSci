0d659bc79be50afd892960171cc782c1cf60d31e
Ed Cashin <ed.cashin@acm.org>
2021-02-16 22:53:41 -0500

    Add a legend

--- GCharts.fs
+++ GCharts.fs
@@ -7,13 +7,12 @@ let googleChartDemo () =
     let rng = SystemRandomSource.Default
     let xy1 = [for i in 1.0..10.0 do i, i * rng.NextDouble()]
     let xy2 = [for i in 1.0..10.0 do i, i + rng.NextDouble()]
-    Chart.Scatter(seq {
-        xy1
-        xy2
-    })
+    Chart.Scatter(seq { xy1; xy2 })
+    |> Chart.WithLabels (seq {"one"; "two"})
     |> Chart.WithOptions(
         Options(
             trendlines = [|Trendline(); Trendline()|]
         )
     )
+    |> Chart.WithLegend true
     |> Chart.Show
